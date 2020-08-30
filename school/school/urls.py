0cac67ce27bd31195203a06ea9dd09736f297ad9
btoffolon <bertrand.toffolon@limayrac.fr>
2020-08-30 09:01:44 +0000

    fix /lycee

--- school/school/urls.py
+++ school/school/urls.py
@@ -18,6 +18,6 @@ from django.urls import include,path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path(r'lycee/', include('lycee.urls')),
+    path(r'', include('lycee.urls'))
 
 ]
